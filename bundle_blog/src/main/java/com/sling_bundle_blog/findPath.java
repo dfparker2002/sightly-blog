37aa835dec801d6b7c132a6acf6f9c2fbde95997
U-MMSDOM\jlsi <jlsi@W4DEUMSY9003007.mms-t-systems.de>
2016-04-22 10:59:49 +0200

    initial commit

---
+++ bundle_blog/src/main/java/com/sling_bundle_blog/findPath.java
@@ -0,0 +1,18 @@
+package com.sling_bundle_blog;
+
+import javax.inject.Inject;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+
+@Model(adaptables=Resource.class)
+public class findPath {
+	@Inject
+	String title;
+	
+	@Inject 
+	String description;
+	
+	public String getTitleAndDescription(){
+		return "title: " + title + ",\n description: " + description;
+	}
+}
