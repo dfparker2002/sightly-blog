14d30793e77bef2a07e2325d9c6b56091990b833
U-MMSDOM\jlsi <jlsi@W4DEUMSY9003007.mms-t-systems.de>
2016-04-22 12:38:55 +0200

    1. Always login as admin, 2. Always write class names with Capital letters because sling does not accept them otherwise

--- bundle_blog/src/main/java/com/sling_bundle_blog/FindPath.java
+++ bundle_blog/src/main/java/com/sling_bundle_blog/FindPath.java
@@ -5,7 +5,7 @@ import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Model;
 
 @Model(adaptables=Resource.class)
-public class findPath {
+public class FindPath {
 	@Inject
 	String title;
 	
