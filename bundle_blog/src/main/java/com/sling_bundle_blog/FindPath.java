05cb0f449348654f6ee4ebc164973a75c769cc16
JulienSiems <juliensiems@gmail.com>
2016-04-24 12:18:27 +0200

    First running prototype of the blog

--- bundle_blog/src/main/java/com/sling_bundle_blog/FindPath.java
+++ bundle_blog/src/main/java/com/sling_bundle_blog/FindPath.java
@@ -1,15 +1,17 @@
 package com.sling_bundle_blog;
 
 import javax.inject.Inject;
+import javax.inject.Named;
+
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Model;
 
 @Model(adaptables=Resource.class)
 public class FindPath {
-	@Inject
+	@Inject @Named("jcr:title")
 	String title;
 	
-	@Inject 
+	@Inject @Named("jcr:description")
 	String description;
 	
 	public String getTitleAndDescription(){
